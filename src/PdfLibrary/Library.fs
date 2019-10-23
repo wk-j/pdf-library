f23a08bd8734f10b5f806ce71c5823cf42aee393
wk-j <somnuk.wk@gmail.com>
2019-10-23 12:54:17 +0700

    Pdf sharp

---
+++ src/PdfLibrary/Library.fs
@@ -0,0 +1,30 @@
+﻿namespace PdfLibrary
+open PdfSharp.Pdf.IO
+open PdfSharp.Pdf
+open System.IO
+open System
+
+type PdfSpliter() =
+
+    do
+        Windows874.Windows874.Register()
+
+    member this.TakePages(filePath: string, startPage, endPage) =
+
+        let tempPath = Path.GetTempPath()
+        let tempName = Guid.NewGuid().ToString("N") + ".pdf"
+
+        let temp = Path.Combine(tempPath, tempName)
+        let input = PdfReader.Open(filePath, PdfDocumentOpenMode.Import)
+        let count = input.PageCount
+
+        if (count > startPage && count > endPage) |> not then
+            (false, "")
+        else
+            let output = new PdfDocument()
+            for i in startPage .. endPage do
+                let page = input.Pages.[i]
+                output.AddPage(page) |> ignore
+
+            output.Save(temp)
+            (true, temp)
---
+++ tests/PdfLibrary.Tests/Tests.fs
@@ -0,0 +1,18 @@
+module Tests
+
+open System
+open Xunit
+open PdfLibrary
+open System.IO
+
+
+[<Fact>]
+let ``My test`` () =
+    let spliter = PdfSpliter()
+    let (ok, file) = spliter.TakePages("../../../../../resource/eng.pdf", 0,4)
+    match ok with
+    | true ->
+        printfn "%A" file
+        Assert.True(File.Exists file)
+    | false ->
+        Assert.True(false)
